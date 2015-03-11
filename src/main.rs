c01eeae8c142f378e2a99203b03d3fae7bff5f07
Jon Haddad <jon@jonhaddad.com>
2015-03-10 23:02:32 -0700

    graph and vertex

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::mem;
+use std::collections::HashMap;
+
+struct Graph {
+	vertices: HashMap<i64, Vertex>,
+}
+
+impl Graph {
+	fn new() -> Graph {
+		let hm: HashMap<i64, Vertex> = HashMap::new();
+		Graph{vertices:hm}
+	}
+	fn add_vertex(&mut self) -> Vertex {
+		let mut v = Vertex{id:0};
+		v
+	}
+}
+struct Vertex {
+	id: i32,
+
+
+}
+
+struct Edge {
+	id: i32,
+}
+
+fn main() {
+	let mut g = Graph::new();
+
+    println!("Hello, world!");
+}
+
+#[test]
+fn test_unsafe_vertex() {
+	let mut g = Graph::new();
+	g.add_vertex();
+	g.add_vertex();
+
+}
