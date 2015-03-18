0b7bd637a5d159bd5ecfe2264d9f44a5e966cd59
Jon Haddad <jon@jonhaddad.com>
2015-03-18 15:51:01 -0700

    edges vector on Vertex coming along

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,8 @@ impl Graph {
 	}
 	fn add_vertex(&mut self) -> VertexProxy {
 		let new_id = 1;
-		let vertex = Vertex{id:new_id};
+		let v = Vertex::new(new_id);
 
-		let mut v = Box::new(vertex);
 		let ptr: *const Box<Vertex> = &v as *const Box<Vertex>;
 
 		self.vertices.insert(new_id, v);
@@ -27,9 +26,19 @@ impl Graph {
 
 struct Vertex {
 	id: i64,
+	edges: *mut Vec<Edge>,
 }
 
 impl Vertex {
+	fn new(id: i64) -> Box<Vertex> {
+
+		let mut edges : Vec<Edge> = Vec::new();
+		let edges_ptr :	*mut Vec<Edge> = &mut edges;
+		let vertex = Vertex{id:id, edges:edges_ptr};
+		let mut v = Box::new(vertex);
+		return v;
+	}
+
 	fn query(self) {
 
 	}
@@ -53,18 +62,22 @@ struct VertexProxy {
 //}
 
 impl VertexProxy {
-	fn add_edge(&self, to_vertex: &VertexProxy) {
+	fn add_edge(& self, to_vertex: &VertexProxy) {
 		unsafe {
-			let in_vertex = &*self.v;
+			let in_vertex = &* self .v;
 			let out_vertex = &*to_vertex.v;
 		}
+		// create the edge
+		let e = Edge{from_vertex: self.v,
+					 to_vertex:to_vertex.v};
+
+
 	}
 }
 
 struct Edge {
-	id: i32,
-	from_vertex: *const Vertex,
-	to_vertex: *const Vertex
+	from_vertex: *const Box<Vertex>,
+	to_vertex: *const Box<Vertex>
 }
 
 fn main() {
