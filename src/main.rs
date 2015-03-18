b231451d71bf032c5b38b001ab893fd3fa5f2453
Jon Haddad <jon@jonhaddad.com>
2015-03-18 14:05:10 -0700

    vertex proxy seems to be legit

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,45 @@ use std::collections::HashMap;
 use std::mem::transmute;
 
 struct Graph {
-	vertices: HashMap<i64, *mut Vertex>,
+	vertices: HashMap<i64, Vertex>,
 }
 
 impl Graph {
 	fn new() -> Box<Graph> {
-		let hm: HashMap<i64, *mut Vertex> = HashMap::new();
+		let hm: HashMap<i64, Vertex> = HashMap::new();
 		Box::new(Graph{vertices:hm})
 	}
-	fn add_vertex(&mut self) -> *mut Vertex {
-		let mut v = Box::new(Vertex{id:0});
+	fn add_vertex(&mut self) -> VertexProxy {
+		let new_id = 1;
+		let vertex = Vertex{id:new_id};
 
-		unsafe {
-			let foo: *mut Vertex = transmute(v);
-			foo
-		}
+		let ptr: *const Vertex = &vertex as *const Vertex;
+		let mut v = Box::new(vertex);
+
+		// return the proxy which knows it's own id
+		VertexProxy{id:new_id, v: ptr}
+	}
+}
+
+struct Vertex {
+	id: i64,
+}
+
+impl Vertex {
+	fn query(self) {
 
 	}
 }
-struct Vertex {
-	id: i32,
 
-
+struct VertexProxy {
+	id: i64,
+	v: *const Vertex,
 }
 
 struct Edge {
 	id: i32,
+	from_vertex: *const Vertex,
+	to_vertex: *const Vertex
 }
 
 fn main() {
@@ -40,7 +53,9 @@ fn main() {
 #[test]
 fn test_unsafe_vertex() {
 	let mut g = Graph::new();
-	g.add_vertex();
-	g.add_vertex();
+	let mut v1 = g.add_vertex();
+	let mut v2 = g.add_vertex();
+
+	v1.test()
 
 }
