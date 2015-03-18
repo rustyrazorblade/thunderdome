b0e5562e09f03a125bc76d4b3b6cdc4d70fcfe70
Jon Haddad <jon@jonhaddad.com>
2015-03-18 15:29:20 -0700

    get in and out vertex

--- src/main.rs
+++ src/main.rs
@@ -40,9 +40,24 @@ struct VertexProxy {
 	v: *const Box<Vertex>,
 }
 
-impl VertexProxy {
-	fn add_edge(&self, v2: &VertexProxy) {
+//let i: u32 = 1;
+//// explicit cast
+//let p_imm: *const u32 = &i as *const u32;
+//let mut m: u32 = 2;
+//// implicit coercion
+//let p_mut: *mut u32 = &mut m;
+//
+//unsafe {
+//let ref_imm: &u32 = &*p_imm;
+//let ref_mut: &mut u32 = &mut *p_mut;
+//}
 
+impl VertexProxy {
+	fn add_edge(&self, to_vertex: &VertexProxy) {
+		unsafe {
+			let in_vertex = &*self.v;
+			let out_vertex = &*to_vertex.v;
+		}
 	}
 }
 
