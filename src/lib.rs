527d3e67f9c2cae7e9836bdd98a4a6e42a74694d
Jon Haddad <jon@jonhaddad.com>
2015-03-21 21:40:36 -0700

    removed pointer help comment

--- src/lib.rs
+++ src/lib.rs
@@ -63,18 +63,6 @@ pub mod graph {
         pub v: *mut Vertex,
     }
 
-    //let i: u32 = 1;
-    //// explicit cast
-    //let p_imm: *const u32 = &i as *const u32;
-    //let mut m: u32 = 2;
-    //// implicit coercion
-    //let p_mut: *mut u32 = &mut m;
-    //
-    //unsafe {
-    //let ref_imm: &u32 = &*p_imm;
-    //let ref_mut: &mut u32 = &mut *p_mut;
-    //}
-
     impl VertexProxy {
         pub fn add_edge(&mut self, to_vertex: &mut VertexProxy) {
             let in_vertex: &mut Vertex;
