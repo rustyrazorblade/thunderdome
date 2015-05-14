ca2c2180473d839d2add022f61b657ab45af58cd
Jon Haddad <jon@jonhaddad.com>
2015-05-14 16:09:08 -0500

    fixed tabs

--- src/traversal.rs
+++ src/traversal.rs
@@ -9,7 +9,7 @@ use path::{Path, Element};
 // define a traversal as a series of stages
 
 pub struct GraphQuery {
-	// many paths
+    // many paths
     pub paths: Vec<Path>
 }
 
@@ -18,7 +18,7 @@ impl GraphQuery {
         // create a new path for each proxy, add the proxy
         let mut paths = Vec::new();
         for p in vertices {
-			let mut path = Path::new(p);
+            let mut path = Path::new(p);
             paths.push(path)
         }
         GraphQuery{paths:paths}
@@ -30,56 +30,56 @@ impl GraphQuery {
     }
 
 
-	pub fn get_paths(&self) -> &Vec<Path> {
-		&self.paths
-	}
-	pub fn push(&mut self, path:Path) {
-		self.paths.push(path)
-	}
+    pub fn get_paths(&self) -> &Vec<Path> {
+        &self.paths
+    }
+    pub fn push(&mut self, path:Path) {
+        self.paths.push(path)
+    }
 
-	/*
-	generic mapper for our queries
-	future this may use worker threads to perform
-	traversals
-	 */
+    /*
+    generic mapper for our queries
+    future this may use worker threads to perform
+    traversals
+     */
     fn map<F: Fn(&Path) -> &[Element]>(&self, closure: F) -> GraphQuery  {
         let mut result = GraphQuery::empty(); // result
-		for path in self.paths.iter() {
-			let mut tmp = closure(path);
+        for path in self.paths.iter() {
+            let mut tmp = closure(path);
             // currently gets back a Vec<Path> but what if it gets Elements?
             let new_elements = path.permute(tmp);
             result.paths.push_all(&new_elements);
         }
-		result
+        result
     }
 
     pub fn outV(&self) -> GraphQuery {
         // needs to be modified to return a Vec of elements or a slice?
         // map can figure out the rest
-		let f = |path: &Path| {
-			println!("applying outV");
-			//take the final element in the path
-			let element = path.last().unwrap();
+        let f = |path: &Path| {
+            println!("applying outV");
+            //take the final element in the path
+            let element = path.last().unwrap();
 
-			let result : &[Element] = match element {
-				&Element::Vertex(ref v) => {
-					v.outV().iter().map(|&v| Element::Vertex(v)).collect()
-				},
-				&Element::Edge(ref e) => {
+            let result : &[Element] = match element {
+                &Element::Vertex(ref v) => {
+                    v.outV().iter().map(|&v| Element::Vertex(v)).collect()
+                },
+                &Element::Edge(ref e) => {
                     &[]
-				}
-			};
-			// apply outV
-			result
-		};
-		self.map(f)
+                }
+            };
+            // apply outV
+            result
+        };
+        self.map(f)
     }
 
-//	pub fn inV(&self) -> GraphQuery {
-//		let g = GraphQuery::empty();
-//		let f = |element: &Element| {
-//			element.inV()
-//		};
-//		self.map(f)
-//	}
+//    pub fn inV(&self) -> GraphQuery {
+//        let g = GraphQuery::empty();
+//        let f = |element: &Element| {
+//            element.inV()
+//        };
+//        self.map(f)
+//    }
 }
