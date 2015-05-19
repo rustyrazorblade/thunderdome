f5e2a79e8bd48a8cb23efc669c8f479f83ae8aea
Jon Haddad <jon@jonhaddad.com>
2015-05-18 23:30:43 -0700

    almost moved all tests out to dedicated files

--- src/lib.rs
+++ src/lib.rs
@@ -15,37 +15,7 @@ mod tests {
     use graph::{Graph, TraversableToVertex};
     use path::{Path, Element};
 
-    #[test]
-    fn test_unsafe_vertex() {
-        let v = RawVertex::new(1);
 
-        let out_edges = &v.out_edges;
-        let in_edges = &v.in_edges;
-    }
-
-    #[test]
-    fn test_deref() {
-        let mut g = Graph::new();
-        let v1 = g.add_vertex();
-        let vertex = &*v1;
-        assert!(vertex.id == 1);
-    }
-
-    #[test]
-    fn test_new_vertex() {
-        let mut g = Graph::new();
-        let v1 = g.add_vertex();
-
-        assert_eq!(v1.id, 1);
-
-        unsafe {
-            let vertex = &*v1;
-            assert_eq!((*v1).id, 1);
-        }
-
-        let v2 = g.add_vertex();
-        assert!(v2.id == 2);
-    }
 
 
 
