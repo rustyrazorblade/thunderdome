095e5367b1656e38501b98cb19ff2e8a9c2992a5
Jon Haddad <jon@jonhaddad.com>
2015-05-18 23:18:57 -0700

    importing properties in submodule

--- src/lib.rs
+++ src/lib.rs
@@ -67,24 +67,7 @@ mod tests {
         //assert_eq!(v1.v, *(e1.edge).from_vertex);
     }
 
-    #[test]
-    fn test_properties() {
-        let mut g = Graph::new();
-        let mut v1 = g.add_vertex();
-        let key = "jon".to_string();
-        let value = Property::Int(1);
-        v1.set_property(key.clone(), value);
 
-        let result = v1.get_property(key).unwrap();
-
-        match result {
-            &Property::Int(1) =>
-                // we're ok
-                println!("OK"),
-            _ =>
-                panic!("wrong graph property")
-        }
-    }
 
     #[test]
     fn test_get_vertex() {
@@ -173,25 +156,5 @@ mod tests {
 
         // check last path
         let path2 = path.permute(&vec);
-
-
-
     }
-    #[test]
-    fn test_has() {
-        let mut g = Graph::new();
-        let mut v = g.add_vertex();
-        let mut v2 = g.add_vertex();
-        v.add_edge(&mut v2, &"friends");
-
-        v2.set_property("favorite_food".to_string(), Property::String("pizza".to_string()));
-
-        let result = g.v(1).outV(&[&"friends"]).
-                        has(&"favorite_food",
-                            Property::String("pizza".to_string()));
-
-        // assert_eq!(result.paths.len(), 1);
-
-    }
-
 }
--- tests/properties.rs
+++ tests/properties.rs
@@ -1,12 +1,50 @@
 extern crate thunderdome;
 
 #[cfg(test)]
-mod tests {
+mod property_tests {
+
+    use thunderdome::graph::*;
+    use thunderdome::property::*;
 
     #[test]
-    fn blah() {
-        assert!(false);
+    fn test_get_set_compare_properties() {
+
     }
 
+    #[test]
+    fn test_properties() {
+        let mut g = Graph::new();
+        let mut v1 = g.add_vertex();
+        let key = "jon".to_string();
+        let value = Property::Int(1);
+        v1.set_property(key.clone(), value);
+
+        let result = v1.get_property(key).unwrap();
+
+        match result {
+            &Property::Int(1) =>
+                // we're ok
+                println!("OK"),
+            _ =>
+                panic!("wrong graph property")
+        }
+    }
+
+    #[test]
+    fn test_has() {
+        let mut g = Graph::new();
+        let mut v = g.add_vertex();
+        let mut v2 = g.add_vertex();
+        v.add_edge(&mut v2, &"friends");
+
+        v2.set_property("favorite_food".to_string(), Property::String("pizza".to_string()));
+
+        let result = g.v(1).outV(&[&"friends"]).
+                        has(&"favorite_food",
+                            Property::String("pizza".to_string()));
+
+        // assert_eq!(result.paths.len(), 1);
+
+    }
 
 }
