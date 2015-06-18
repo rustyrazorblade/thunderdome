9985c842b12a084dfed395bd76174a455c39dcb7
Jon Haddad <jon@jonhaddad.com>
2015-06-18 00:00:35 -0400

    hello world up on /

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,19 @@ use conduit::{Request, Response};
 use conduit_router::{RouteBuilder, RequestParams};
 use civet::{Config, response, Server};
 use std::sync::mpsc::channel;
+use std::io::{self, Cursor};
+use std::collections::HashMap;
+
+fn hello(_req: &mut Request) -> io::Result<Response> {
+    Ok(response(200, HashMap::new(), "Thunderdome graph server, Hello World!".as_bytes()))
+}
 
 fn main() {
     println!("Thunderdome Graph Server starting up...");
 
     let mut router = RouteBuilder::new();
+    router.get("/", hello);
+
     let server = Server::start(Config { port: 8888, threads: 1 }, router);
 
     // Preventing process exit.
