4f9466cd6af3b593b2575ec6916216cc3555d9da
Jon Haddad <jon@jonhaddad.com>
2015-06-21 14:54:46 -0400

    adding steps to peg

--- src/steps.rs
+++ src/steps.rs
@@ -1,9 +1,19 @@
 pub struct GraphQuery {
-    pub blah: i64,
     pub scope: Scope,
+    pub steps: Vec<Step>
 }
 
 pub enum Scope {
     Global,
     Vertex(Vec<i64>),
+}
+
+pub struct Step {
+    pub name: String
+}
+
+impl Step {
+    pub fn new(name: String) -> Step {
+        Step{name:name}
+    }
 }
