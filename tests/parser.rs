72dec3c81d0fdab0ee1f376c80419aa92d303171
Jon Haddad <jon@jonhaddad.com>
2015-06-21 16:13:07 -0400

    compacted validation

--- tests/parser.rs
+++ tests/parser.rs
@@ -2,28 +2,33 @@
 
 extern crate thunderdome;
 
+
+
 #[cfg(test)]
 mod parser_tests {
     use thunderdome::parser::*;
+    use thunderdome::steps::*;
+
+    fn validate(q: &str) -> Option<GraphQuery> {
+        let result = parse(q);
+        assert!(result.is_ok());
+        result.ok()
+    }
 
     #[test]
     fn global_graph_query() {
-        parse("g");
-        let result = parse("g.V");
-        assert!(result.is_ok());
-
+        validate("g.V");
     }
+
     #[test]
     fn vertex_query() {
-        let r = parse("g.v(1)");
-        assert!(r.is_ok());
-        let r = parse("g.v(1,2)");
-        assert!(r.is_ok());
-        assert!(parse("g.v(1, 2)").is_ok());
+        validate("g.v(1)");
+        validate("g.v(1,2)");
+        validate("g.v(1, 2)");
     }
 
     #[test]
     fn simple_step_test() {
-        assert!(parse("g.v(1).outV()").is_ok());
+        validate("g.v(1).outV()");
     }
 }
