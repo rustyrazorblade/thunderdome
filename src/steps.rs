74469a0e8c99e5385bf8a9f4b45d47c68881f224
Jon Haddad <jon@jonhaddad.com>
2015-06-22 13:51:00 -0400

    validating that the steps are named correctly and in the right order

--- src/steps.rs
+++ src/steps.rs
@@ -8,11 +8,19 @@ pub struct ParsedGraphQuery {
     pub steps: Vec<Step>
 }
 
+/*
+scope of the query.  determines if we're looking at the
+entire graph or just from a handful of vertices
+*/
 pub enum Scope {
     Global,
     Vertex(Vec<i64>),
 }
 
+/*
+generic step used in ParsedGraphQuery
+will be turned into specific steps
+*/
 pub struct Step {
     pub name: String
 }
