7d860ed399517795b2158fff019d83d862921808
Jon Haddad <jon@jonhaddad.com>
2015-08-21 09:40:43 -0700

    comparison ok

--- tests/properties.rs
+++ tests/properties.rs
@@ -12,11 +12,16 @@ fn test_properties() {
     // let mut g = Graph::new();
     // let mut v1 = g.add_vertex();
     let mut p = PropertyMap::new();
-    p.set("key".to_string(), Box::new("Value".to_string()));
-    p.set("key2".to_string(), Box::new(1));
 
-    let tmp = p.get::<String>("key".to_string()).unwrap();
-    assert_eq!(tmp, "Value");
+    let (k, v) = ("key".to_string(), "Value".to_string());
+    p.set(k, Box::new(v));
+
+    {
+        let tmp = p.get::<String>("key".to_string()).unwrap();
+        assert_eq!(tmp, "Value");
+    }
+
+    p.set("key2".to_string(), Box::new(1));
 
 
 }
