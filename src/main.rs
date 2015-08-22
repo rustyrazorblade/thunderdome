5e1fd5ad06a18f8a98d34b3e6f66436c5738da81
Jon Haddad <jon@jonhaddad.com>
2015-08-22 10:25:06 -0700

    logger working

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ extern crate capnpc;
 
 #[macro_use]
 extern crate log;
+extern crate env_logger;
+
 
 use std::io::Read;
 use std::io::{self, Cursor};
@@ -22,22 +24,32 @@ mod thunderdome_capnp {
 use thunderdome_capnp::graph;
 
 fn main() {
-    println!("Thunderdome Graph Database Server starting up...");
+    /*
+    Logger notes:
+    RUST_LOG=error ./main
+    RUST_LOG=info
+
+    http://rust-lang.github.io/log/env_logger/
+    
+    */
+    env_logger::init().unwrap();
+
+    info!("Thunderdome Graph Database Server starting up...");
 
     // create an instance of the graph, wrap in RefCell / rc and let it do it's thing
     let g = Graph::new();
-    println!("Graph up");
+    info!("Graph up");
 
     let tx = g.execution_channel();
 
-    println!("open execution channel");
+    info!("open execution channel");
 
     let listener = TcpListener::bind("127.0.0.1:6000").unwrap();
 
-    println!("Socket bound");
+    info!("Socket bound");
 
     for stream in listener.incoming() {
-        println!("connection established, spawning new thread");
+        info!("connection established, spawning new thread");
         let new_tx = tx.clone();
         match stream {
             Ok(stream) => {
@@ -54,7 +66,7 @@ fn main() {
 }
 
 fn handle_client(mut stream: TcpStream, tx: Sender<Request> ) {
-    println!("connected.  opening local channel for graph comm and creating request context");
+    info!("connected.  opening local channel for graph comm and creating request context");
 
     // let (client_tx, client_rx) = channel();
     // let mut buffer = String::new();
@@ -63,14 +75,14 @@ fn handle_client(mut stream: TcpStream, tx: Sender<Request> ) {
         // read the header - 4 bytes?
         match  stream.read_to_end(&mut buffer) {
             Ok(0) => {
-                println!("no bytes received bailing out");
+                info!("no bytes received bailing out");
                 return ()
             }
             Ok(bytes) => {
-                println!("ok - command received {}", bytes);
+                info!("ok - command received {}", bytes);
             },
             Err(_) => {
-                println!("Error, leaving");
+                info!("Error, leaving");
                 return ();
             }
         };
